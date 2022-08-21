f8de0124ace643313014e947d8d613d3bd7559aa
Jaysinh Dabhi <87559228+JaysinhDabhi@users.noreply.github.com>
2022-08-21 09:20:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Website-views-increase-script
+
+# Website view incease Script
+
+This script basically incease Website views will help for grow your adsence earning.
+
+
+## Features
+
+- Increase views of website
+- Grow your adsence earning
+- boost your website traffic
+
+
+## Usage/Examples
+
+```javascript
+Example:-
+
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL"
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "https://adaptedtech.blogspot.com/ "
+
+
+```
+
+
+## Authors
+
+- [@Jaysinh Dabhi](https://github.com/JaysinhDabhi/)
+
---
+++ Website view increase script by Jaysinh Dabhi.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+Echo Instagram-@imjaysinhdabhi
+Echo Github-jaysinhdabhi
+
+:LOOP
+
+
+
+
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+
+
+GOTO LOOP
+
+EXIT
